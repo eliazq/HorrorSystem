fae8524b654f34b5cbb42a29713f616a2d8c8664
Elias H <69460338+eliazq@users.noreply.github.com>
2024-07-10 19:49:52 +0300

    HandHeadBob Script Update

--- Assets/Scripts/Player/HandHeadBob.cs
+++ Assets/Scripts/Player/HandHeadBob.cs
@@ -7,14 +7,22 @@ public class HandHeadBob : MonoBehaviour
 {
     [SerializeField] private float handBobStopTime = 3f;
     [Range(0.001f, 0.1f)]
-    [SerializeField] private float walkingAmount = 0.002f; 
+    [SerializeField] private float idleAmount = 0.004f;
     [Range(1f, 30f)]
-    [SerializeField] private float walkingFrequency = 10.0f; 
+    [SerializeField] private float idleFrequency = 1.0f;
     [Range(0.001f, 0.1f)]
-    [SerializeField] private float runningAmount = 0.002f; 
+    [SerializeField] private float aimingAmount = 0.001f;
+    [Range(1f, 30f)]
+    [SerializeField] private float aimingFrequency = 2.9f;
+    [Range(0.001f, 0.1f)]
+    [SerializeField] private float walkingAmount = 0.035f; 
+    [Range(1f, 30f)]
+    [SerializeField] private float walkingFrequency = 10.0f;
+    [Range(0.001f, 0.1f)]
+    [SerializeField] private float runningAmount = 0.025f; 
 
     [Range(1f, 30f)]
-    [SerializeField] private float runningFrequency = 10.0f; 
+    [SerializeField] private float runningFrequency = 19.6f; 
 
     private float Amount = 0; 
 
@@ -26,7 +34,13 @@ public class HandHeadBob : MonoBehaviour
     [SerializeField] private float walkSmooth = 10.0f; 
 
     [Range(0f, 100f)]
-    [SerializeField] private float runningSmooth = 10.0f; 
+    [SerializeField] private float runningSmooth = 10.0f;
+
+    [Range(0, 100f)]
+    [SerializeField] private float idleSmooth = 10.0f;
+
+    [Range(0, 100f)]
+    [SerializeField] private float aimingSmooth = 10.0f;
 
     [SerializeField] private float LeanAmount = 1.8f;
     [SerializeField] private float LeanTime = 3f;
@@ -35,15 +49,7 @@ public class HandHeadBob : MonoBehaviour
     Quaternion StartRot;
     [SerializeField] FirstPersonController firstPersonController;
     bool isWalkingSettings = true;
-
-    [Header("Breathing In Aiming")]
-    [SerializeField] private float breathingMovementRadius = 0.01f; // Radius of the circle
-    [SerializeField] private float breathingMovementSpeed = 2f; // Speed of movement
-
-    private Vector3 breathingMovementCenter = Vector3.zero; // Center of the circle
-    private Vector3 breathingMovementTargetPosition;
-
-    bool SetFirstTime = true;
+    bool setFirstTime = true;
 
     void Start()
     {
@@ -52,8 +58,6 @@ public class HandHeadBob : MonoBehaviour
 
         if (firstPersonController == null) 
             firstPersonController = GetComponentInParent<FirstPersonController>();
-        breathingMovementCenter = StartPos;
-        GetNewBreathingTargetPosition();
         SetWalkingHeadBobSettings();
     }
 
@@ -64,20 +68,18 @@ public class HandHeadBob : MonoBehaviour
             CheckForHeadbobTrigger();
             StopHeadbob();
             CameraLean();
-            SetFirstTime = true;
+        }
+        else if (!Player.Instance.WeaponHandling.IsAiming)
+        {
+            CheckForHeadbobTrigger();
+            StopHeadbob();
+            CameraLean();
         }
         else
         {
-            BreathingHeadBob();
+            CheckForHeadbobTrigger();
+            StopHeadbob();
             CameraLean();
-        }
-    }
-
-    private void BreathingHeadBob()
-    {
-        if (Player.Instance.WeaponHandling.IsAiming)
-        {
-            MoveTowardsBreathingTarget();
         }
     }
 
@@ -125,6 +127,16 @@ public class HandHeadBob : MonoBehaviour
             if (isWalkingSettings) SetRunningHeadBobSettings();
             StartHeadBob();
         }
+        else if (!Player.Instance.WeaponHandling.IsAiming)
+        {
+            SetIdleHeadBobSettings();
+            StartHeadBob();
+        }
+        else
+        {
+            SetAimingHeadBobSettings();
+            StartHeadBob();
+        }
     }
 
     private void SetWalkingHeadBobSettings()
@@ -143,6 +155,21 @@ public class HandHeadBob : MonoBehaviour
         isWalkingSettings = false;
     }
 
+    private void SetIdleHeadBobSettings()
+    {
+        Amount = idleAmount;
+        Frequency = idleFrequency;
+        Smooth = idleSmooth;
+        isWalkingSettings = false;
+    }
+    private void SetAimingHeadBobSettings()
+    {
+        Amount = aimingAmount;
+        Frequency = aimingFrequency;
+        Smooth = aimingSmooth;
+        isWalkingSettings = false;
+    }
+
     private Vector3 StartHeadBob()
     {
         Vector3 pos = Vector3.zero;
@@ -156,25 +183,6 @@ public class HandHeadBob : MonoBehaviour
     {
         if (transform.localPosition == StartPos) return;
         transform.localPosition = Vector3.Slerp(transform.localPosition, StartPos, handBobStopTime * Time.deltaTime);
-    }
-
-    void GetNewBreathingTargetPosition()
-    {
-        SetFirstTime = false;
-        breathingMovementTargetPosition = new Vector3(StartPos.x + Random.Range(-breathingMovementRadius, breathingMovementRadius),
-            StartPos.y + Random.Range(-breathingMovementRadius, breathingMovementRadius), transform.localPosition.z);
-    }
-
-    void MoveTowardsBreathingTarget()
-    {
-        if (SetFirstTime) transform.localPosition = breathingMovementTargetPosition;
-        transform.localPosition = Vector3.MoveTowards(transform.localPosition, breathingMovementTargetPosition, breathingMovementSpeed * Time.deltaTime);
-
-        // Check if the object has reached the target position
-        if (Vector3.Distance(transform.localPosition, breathingMovementTargetPosition) <= 0.001f)
-        {
-            GetNewBreathingTargetPosition();
-        }
     }
 
 }
